1459b7eae37fba1ae51aa90a595eb78ef2d7a9b1
JesseOSullivan <105776319+JesseOSullivan@users.noreply.github.com>
2023-10-01 13:13:08 +1000

    s

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -29,7 +29,7 @@ const Navbar = () => {
         <img 
       src="/nav-logo.svg" 
       alt="Logo" 
-      style={{ maxWidth: '400px', height: 'auto' }} 
+      style={{ maxWidth: '500px', height: 'auto' }} 
     />
       </a>
         </div>
